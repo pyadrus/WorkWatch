705b007e5cd1cd00ec1534d2e8a2d86e1554d1be
PyAdminRU <zh.vitaliy92@gmail.com>
2025-05-28 00:16:05 +0300

    Формирование проекта

--- keyboards.py
+++ keyboards.py
@@ -4,4 +4,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def start_keyboard():
     """Клавиатура главного меню бота"""
-    return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Справка', callback_data='reference'),],])
+    return InlineKeyboardMarkup(
+        inline_keyboard=[[InlineKeyboardButton(text='Справка', callback_data='reference'),],
+                         [InlineKeyboardButton(text='✅ На работе', callback_data='at_work'), InlineKeyboardButton(text="🏠 Ушёл", callback_data="left")]]
+    )
